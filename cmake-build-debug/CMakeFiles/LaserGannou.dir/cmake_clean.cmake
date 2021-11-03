7493a6aa47fce2fc00372e8e462fd122cc66ff50
rowanross <rowan.ross@student.hu.nl>
2021-11-03 14:14:37 +0100

    Clion project

---
+++ cmake-build-debug/CMakeFiles/LaserGannou.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LaserGannou.dir/main.cpp.obj"
+  "LaserGannou.exe"
+  "LaserGannou.exe.manifest"
+  "LaserGannou.pdb"
+  "libLaserGannou.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LaserGannou.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
