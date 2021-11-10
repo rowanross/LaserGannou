7d60ce42ce9ba17add1e021fbeffd908afd6dd8b
jurgen1507 <jbraan01>
2021-11-10 14:31:30 +0100

    transferHit update

--- transferHitControl.hpp
+++ transferHitControl.hpp
@@ -1,47 +1,16 @@
 #ifndef TRANSFERHITCONTROL_HPP
 #define TRANSFERHITCONTROL_HPP
-
+#include "runGameControl.h"
 #include <array>
 class transferHit : public rtos::task<>{
-private:
-    bool nextGame;
-    void main(){
-        enum state_t {BUSY, FINISHED}
-        state_t state = BUSY;
-        rtos::flag acceptButton;
-
-        for (;;){
-            switch(state){
-                case BUSY: {
-                    if (gameTime == 0){
-                        state = FINISHED;
-                    }
-                    break;
-                }
-
-                case FINISHED:{
-                    auto evt = wait(acceptButton);
-                    if(evt == acceptButton){
-                        hwlib::wait_ms(200);
-                        for (std::array & x : a){
-                            hwlib::cout << playernr << ","<< hits << "\n";
-                        }
-
-                        state = BUSY;
-
-                    }
-                    break;
-                }
-            }
+public:
+    transferHit();
+    void transferData(const killedBy kills[9]){
+        hwlib::wait_ms(200);
+        for (const auto & x : kills){
+            hwlib::cout << x.playerID << ","<< x.amount << "\n";
         }
     }
-
-public:
-    transferHit():
-        rtos::task<>("TransferHit"),
-        acceptButton(this, "acceptButton")
-    {}
-
 };
 
 
