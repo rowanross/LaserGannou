277ad0020d1a3bb35a8d6fe36f502a9d6d12532f
KyrillW <KyrillW@hotmail.com>
2021-11-10 23:01:40 +0100

    Update transferHitControl.hpp

--- transferHitControl.hpp
+++ transferHitControl.hpp
@@ -9,7 +9,7 @@ public:
     transferHit();
     void transferData(const killedBy kills[9]){
         hwlib::wait_ms(200);
-        for(unsigned int i = 0; i < 8; i++){
+        for(unsigned int i = 0; i < 9; i++){
             hwlib::cout << kills[i].playerCode << ","<< kills[i].amount << "\n";
         }
 //        for (const killedBy & x : kills){
